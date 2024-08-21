53516799929a8c6f1c140391a20559818da44d4b
Henry Toone <hello@henrytoone.com>
2024-08-21 21:06:51 +0100

    Start again

--- client/src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Home } from './Home'
-export { default as Projects } from './Projects'
-export { default as Contact } from './Contact'
-export { default as NotFound} from "./NotFound"
